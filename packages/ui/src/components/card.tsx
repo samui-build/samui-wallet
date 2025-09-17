d3fff28b206fd2cdfa7a60fcff92281a104ac0d9
Bram Borggreve <beeman@beeman.nl>
2025-09-17 19:08:40 +0300

    feat: add more components

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -50,4 +50,5 @@ function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
 function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
   return <div data-slot="card-footer" className={cn('flex items-center px-6 [.border-t]:pt-6', className)} {...props} />
 }
+
 export { Card, CardHeader, CardFooter, CardTitle, CardAction, CardDescription, CardContent }
--- packages/ui/src/components/select.tsx
+++ packages/ui/src/components/select.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import * as React from 'react'
 import * as SelectPrimitive from '@radix-ui/react-select'
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from 'lucide-react'
+
 import { cn } from '@workspace/ui/lib/utils'
 
 function Select({ ...props }: React.ComponentProps<typeof SelectPrimitive.Root>) {
