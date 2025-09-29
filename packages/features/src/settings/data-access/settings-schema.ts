3287d55f4a22f56f0fdfcbdeda2f36cbaf2c654c
Bram Borggreve <beeman@beeman.nl>
2025-09-30 01:20:31 +0300

    wip: start settings schema

---
+++ packages/features/src/settings/data-access/settings-schema.ts
@@ -0,0 +1,17 @@
+import { Schema } from 'effect'
+
+//
+console.log(Schema)
+
+const SettingsSchema = Schema.Struct({
+  autoLockEnabled: Schema.BooleanFromString,
+  autoLockSeconds: Schema.NumberFromString,
+  // TODO: Make this an enum of USD/EUR/etc
+  currency: Schema.String,
+  // TODO: Make this an enum of EN/ES
+  language: Schema.String,
+  age: Schema.NumberFromString,
+})
+
+export type Settings = Schema.Schema.Type<typeof SettingsSchema>
+export type SettingsEncoded = Schema.Schema.Encoded<typeof SettingsSchema>
