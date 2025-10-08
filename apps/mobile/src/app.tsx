4a6eda1a19b91f78ff65a1543d69b4057115a03f
Tobey Codes <tobey@tobey.codes>
2025-10-07 20:32:03 -0600

    feat: add mobile scaffold

---
+++ apps/mobile/src/app.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export function App() {
+  return (
+    <View>
+      <Text>Samui Wallet</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
