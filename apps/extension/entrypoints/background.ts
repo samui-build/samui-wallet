47b83fbc548324117896829d167ee85099c93f8c
Tobey Codes <tobey@tobey.codes>
2025-10-12 23:48:44 -0400

    feat: add proxy service

--- apps/extension/entrypoints/background.ts
+++ apps/extension/entrypoints/background.ts
@@ -1,5 +1,7 @@
 import { handlers } from '@workspace/background/background'
+import { registerDbService } from '@workspace/background/services/db'
 
 export default defineBackground(() => {
+  registerDbService()
   handlers()
 })
