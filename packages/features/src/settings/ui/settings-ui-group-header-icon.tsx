d857fca3824e0a66d802ace95411245fd2549b8e
Bram Borggreve <beeman@beeman.nl>
2025-10-17 00:52:01 +0400

    chore: remove old settings code

--- packages/features/src/settings/ui/settings-ui-group-header-icon.tsx
+++
@@ -1,5 +0,0 @@
-import type { SettingsGroup } from '../data-access/settings-group.js'
-
-export function SettingsUiGroupHeaderIcon({ group: { icon: Icon } }: { group: SettingsGroup }) {
-  return <Icon className="h-6 w-6" />
-}
