06d4a281350ccf361557822014774c51c30623c4
Tobey Codes <tobey@tobey.codes>
2025-10-15 22:49:24 -0400

    feat: add request service

--- packages/background/src/actions/connect.ts
+++ packages/background/src/actions/connect.ts
@@ -1,7 +1,9 @@
 import type { StandardConnectInput, StandardConnectOutput } from '@wallet-standard/core'
 
+import { getRequestService } from '../services/request'
+
 export async function connect(input?: StandardConnectInput): Promise<StandardConnectOutput> {
   console.log('Connect', input)
 
-  return { accounts: [] }
+  return await getRequestService().create('connect', input)
 }
