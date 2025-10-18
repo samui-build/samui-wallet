e1eb4ac3f78e89eb0237bae803e50abf43e187bb
Bram Borggreve <beeman@beeman.nl>
2025-10-19 02:32:54 +0400

    feat: make sure to set the activeAccountId when creating the first Account

--- packages/settings/src/ui/settings-ui-account-list.tsx
+++ packages/settings/src/ui/settings-ui-account-list.tsx
@@ -6,16 +6,26 @@ import { ItemGroup } from '@workspace/ui/components/item'
 import { SettingsUiAccountListItem } from './settings-ui-account-list-item.js'
 
 export function SettingsUiAccountList({
+  activeAccountId,
   deleteItem,
   items,
+  setActive,
 }: {
+  activeAccountId: null | string
   deleteItem: (item: Account) => Promise<void>
   items: Array<{ wallets?: Wallet[] } & Account>
+  setActive: (item: Account) => Promise<void>
 }) {
   return (
     <ItemGroup className="gap-4">
       {items.map((item) => (
-        <SettingsUiAccountListItem deleteItem={deleteItem} item={item} key={item.id} />
+        <SettingsUiAccountListItem
+          activeAccountId={activeAccountId}
+          deleteItem={deleteItem}
+          item={item}
+          key={item.id}
+          setActive={setActive}
+        />
       ))}
     </ItemGroup>
   )
