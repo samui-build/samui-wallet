e7b7833f17ce46efad2c76aaad88804f8b16b26c
Bram Borggreve <beeman@beeman.nl>
2025-10-20 19:59:41 +0400

    feat: implement solana-client-react package

---
+++ packages/solana-client-react/src/use-solana-client.tsx
@@ -0,0 +1,8 @@
+import type { Cluster } from '@workspace/db/entity/cluster'
+
+import { createSolanaClient } from '@workspace/solana-client/create-solana-client'
+import { useMemo } from 'react'
+
+export function useSolanaClient({ cluster }: { cluster: Cluster }) {
+  return useMemo(() => createSolanaClient({ url: cluster.endpoint }), [cluster.endpoint])
+}
