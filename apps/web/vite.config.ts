0d3965c8ab7ded921e403b655d06590cfb655493
Tobey Codes <tobey@tobey.codes>
2025-10-22 10:07:33 -0600

    test: improve test config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [cloudflare(), react(), tailwindcss(), tsconfigPaths()],
 })
