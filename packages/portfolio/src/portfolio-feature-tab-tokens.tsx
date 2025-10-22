8017a9a31f206d0e5cd7e92ffc88e24f45297bdc
Bram Borggreve <beeman@beeman.nl>
2025-10-23 00:00:35 +0400

    feat: add token metadata using Jupiter API

--- packages/portfolio/src/portfolio-feature-tab-tokens.tsx
+++ packages/portfolio/src/portfolio-feature-tab-tokens.tsx
@@ -1,15 +1,34 @@
+import { useMemo } from 'react'
+
 import type { ClusterWallet } from './portfolio-routes-loaded.js'
 
-import { PortfolioFeatureGetSolBalance } from './portfolio-feature-get-sol-balance.js'
-import { PortfolioFeatureGetTokenAccounts } from './portfolio-feature-get-token-accounts.js'
+import { useGetTokenBalances } from './data-access/use-get-token-metadata.js'
+import { PortfolioUiTokenBalances } from './ui/portfolio-ui-token-balances.js'
 import { PortfolioUiWalletButtons } from './ui/portfolio-ui-wallet-buttons.js'
 
 export function PortfolioFeatureTabTokens(props: ClusterWallet) {
+  const balances = useGetTokenBalances(props)
+
+  const totalBalance = useMemo(() => {
+    const balance = balances.reduce((acc, item) => {
+      if (!item.metadata?.usdPrice) {
+        return acc
+      }
+      const itemBalance = (Number(item.balance) / 10 ** item.decimals) * item.metadata.usdPrice
+      return acc + itemBalance
+    }, 0)
+
+    return new Intl.NumberFormat('en-US', {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 0,
+    }).format(balance)
+  }, [balances])
+
   return (
     <div className="p-4 space-y-6">
-      <PortfolioFeatureGetSolBalance {...props} />
+      <div className="text-4xl font-bold text-center">$ {totalBalance}</div>
       <PortfolioUiWalletButtons {...props} />
-      <PortfolioFeatureGetTokenAccounts {...props} />
+      <PortfolioUiTokenBalances items={balances} />
     </div>
   )
 }
