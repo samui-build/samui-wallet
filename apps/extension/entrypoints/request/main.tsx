fb4329547b8fe56f0d4ce2abf9ed9807299ad814
Tobey Codes <tobey@tobey.codes>
2025-10-23 15:20:02 -0600

    feat(extension): add ui package

--- apps/extension/entrypoints/request/main.tsx
+++ apps/extension/entrypoints/request/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@workspace/ui/globals.css'
 
 import { App } from './app'
 
