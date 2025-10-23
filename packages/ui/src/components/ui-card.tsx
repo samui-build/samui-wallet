932c0b96bc182aad77fadbc268a239364de9a189
beeman <beeman@beeman.nl>
2025-10-23 10:08:57 +0400

    feat: add ui-card component

---
+++ packages/ui/src/components/ui-card.tsx
@@ -0,0 +1,40 @@
+import type { ComponentProps, ReactNode } from 'react'
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@workspace/ui/components/card'
+
+export function UiCard({
+  cardProps,
+  children,
+  contentProps,
+  description,
+  descriptionProps,
+  footer,
+  footerProps,
+  headerProps,
+  title,
+  titleProps,
+}: {
+  cardProps?: Omit<ComponentProps<typeof Card>, 'children'>
+  children: ReactNode
+  contentProps?: Omit<ComponentProps<typeof CardContent>, 'children'>
+  description?: ReactNode
+  descriptionProps?: Omit<ComponentProps<typeof CardDescription>, 'children'>
+  footer?: ReactNode
+  footerProps?: Omit<ComponentProps<typeof CardFooter>, 'children'>
+  headerProps?: Omit<ComponentProps<typeof CardHeader>, 'children'>
+  title?: ReactNode
+  titleProps?: Omit<ComponentProps<typeof CardTitle>, 'children'>
+}) {
+  return (
+    <Card {...cardProps}>
+      {title || description ? (
+        <CardHeader {...headerProps}>
+          {title ? <CardTitle {...titleProps}>{title}</CardTitle> : null}
+          {description ? <CardDescription {...descriptionProps}>{description}</CardDescription> : null}
+        </CardHeader>
+      ) : null}
+      <CardContent {...contentProps}>{children}</CardContent>
+      {footer ? <CardFooter {...footerProps}>{footer}</CardFooter> : null}
+    </Card>
+  )
+}
