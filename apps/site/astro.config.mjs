0aed5dead7425907bd565036745c6cd1948e3973
Derlys Dominguez <derlys275@gmail.com>
2025-10-23 19:22:01 +0400

    feat: add basic site with Starlight and Tailwind

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,5 +1,35 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Samui",
+      favicon: "./src/assets/logo.svg",
+      logo: {
+        src: "./src/assets/logo.svg",
+      },
+      social: [
+        {
+          icon: "x.com",
+          label: "X / Twitter",
+          href: "https://x.com/SamuiBuild",
+        },
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/samui-build/samui-wallet",
+        },
+      ],
+
+      customCss: ["./src/styles/global.css"],
+
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
