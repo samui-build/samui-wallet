fb46d28274d39646f515b184ebcecd2802de3032
leopham <hongthaipro@gmail.com>
2025-10-27 23:22:38 +0700

    feat: enhance spl tranfer

--- packages/solana-client/src/create-spl-transfer-transaction.ts
+++ packages/solana-client/src/create-spl-transfer-transaction.ts
@@ -1,10 +1,7 @@
 import type { Address, Blockhash, Instruction, TransactionSigner } from '@solana/kit'
 
-import {
-  getCreateAssociatedTokenInstruction,
-  getTransferCheckedInstruction,
-  TOKEN_PROGRAM_ADDRESS,
-} from '@solana-program/token'
+import { TOKEN_PROGRAM_ADDRESS } from '@solana-program/token'
+import { getCreateAssociatedTokenInstruction, getTransferCheckedInstruction } from '@solana-program/token-2022'
 import {
   address,
   appendTransactionMessageInstructions,
