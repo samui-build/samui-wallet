71392258c3094e2879a2780e7ae0d5aa2bff1d62
Tobey Codes <tobey@tobey.codes>
2025-10-27 18:26:04 -0600

    feat(extension): sign transaction

--- packages/background/src/actions/sign-transaction.ts
+++ packages/background/src/actions/sign-transaction.ts
@@ -1,7 +1,7 @@
 import type { SolanaSignTransactionInput, SolanaSignTransactionOutput } from '@solana/wallet-standard-features'
 
-export async function signTransaction(inputs: SolanaSignTransactionInput[]): Promise<SolanaSignTransactionOutput[]> {
-  console.log('Sign Transaction', inputs)
+import { getRequestService } from '../services/request'
 
-  return Promise.resolve([])
+export async function signTransaction(inputs: SolanaSignTransactionInput[]): Promise<SolanaSignTransactionOutput[]> {
+  return await getRequestService().create('signTransaction', inputs)
 }
