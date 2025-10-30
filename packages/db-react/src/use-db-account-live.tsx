cd4dbac08684b13667fb47e2e9ee720dc44e2862
Bram Borggreve <beeman@beeman.nl>
2025-10-30 17:38:31 +0400

    fix(shell): useDbAccountLive hook for account list
    This should fix the issue where the Account menu does not
    get updated when new wallets or accounts are added.

--- packages/db-react/src/use-db-account-live.tsx
+++ packages/db-react/src/use-db-account-live.tsx
@@ -1,8 +1,9 @@
 import type { Account } from '@workspace/db/entity/account'
 
 import { db } from '@workspace/db/db'
+import { dbAccountFindMany } from '@workspace/db/db-account-find-many'
 import { useLiveQuery } from 'dexie-react-hooks'
 
 export function useDbAccountLive() {
-  return useLiveQuery<Account[], Account[]>(() => db.accounts.orderBy('name').toArray(), [], [])
+  return useLiveQuery<Account[], Account[]>(() => dbAccountFindMany(db), [], [])
 }
--- packages/shell/src/ui/shell-ui-menu.tsx
+++ packages/shell/src/ui/shell-ui-menu.tsx
@@ -1,6 +1,4 @@
-import type { Account } from '@workspace/db/entity/account'
-
-import { useDbAccountFindMany } from '@workspace/db-react/use-db-account-find-many'
+import { useDbAccountLive } from '@workspace/db-react/use-db-account-live'
 import { useDbClusterLive } from '@workspace/db-react/use-db-cluster-live'
 import { useDbPreference } from '@workspace/db-react/use-db-preference'
 import { getDevOptions } from '@workspace/dev/dev-features'
@@ -16,12 +14,10 @@ import { ShellUiMenuDevelopment } from './shell-ui-menu-development.js'
 export function ShellUiMenu() {
   const { active: activeAccount } = useActiveAccount()
   const { active: activeWallet, setActive: setActiveWalletId } = useActiveWallet()
-  const accountsQuery = useDbAccountFindMany({ input: {} })
+  const accounts = useDbAccountLive()
   const items = useDbClusterLive()
   const [activeId, setActiveClusterId] = useDbPreference('activeClusterId')
   const activeCluster = useMemo(() => items.find((c) => c.id === activeId), [items, activeId])
-
-  const accounts: Account[] = accountsQuery.data ?? []
 
   return (
     <Menubar className="py-2 h-10 md:h-14 bg-transparent border-none">
