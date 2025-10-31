bd2702d671a92a4ab8b45ac81cb5c16448710d14
Bram Borggreve <beeman@beeman.nl>
2025-10-31 14:10:34 +0400

    fix(site): remove astro getStaticPaths warning in dev mode
    [WARN] [router] A `getStaticPaths()` route pattern was matched, but no
    matching static path was found for requested path `/go/discord`.

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -33,7 +33,23 @@ export default defineConfig({
       customCss: ['./src/styles/global.css'],
     }),
   ],
+  redirects: {
+    // These redirects also exist in /public/_redirects.
+    '/go/discord': {
+      destination: 'https://discord.gg/x6WcQ7bVgF',
+      status: 302,
+    },
+    '/go/github': {
+      destination: 'https://github.com/samui-build/samui-wallet',
+      status: 302,
+    },
+    '/go/x': {
+      destination: 'https://x.com/SamuiBuild',
+      status: 302,
+    }
+  },
   vite: {
+    // @ts-expect-error Astro 5 uses Vite 6, we use Vite 7 in our monorepo
     plugins: [tailwindcss()],
   },
 })
