d5cbf0f60932c4ff664bd82c5a33d56fdf48c0a2
Tobey Codes <tobey@tobey.codes>
2025-10-31 09:29:50 -0600

    feat: invalidate queries on sending sol or spl tokens

--- packages/portfolio/src/use-create-and-send-spl-transaction.tsx
+++ packages/portfolio/src/use-create-and-send-spl-transaction.tsx
@@ -1,13 +1,18 @@
 import type { Wallet } from '@workspace/db/entity/wallet'
 
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createKeyPairSignerFromJson } from '@workspace/keypair/create-key-pair-signer-from-json'
+import { getAccountInfoQueryOptions } from '@workspace/solana-client-react/use-get-account-info'
+import { getBalanceQueryOptions } from '@workspace/solana-client-react/use-get-balance'
+import { getTokenAccountsQueryOptions } from '@workspace/solana-client-react/use-get-token-accounts'
 import { useSolanaClient } from '@workspace/solana-client-react/use-solana-client'
 import { createAndSendSplTransaction } from '@workspace/solana-client/create-and-send-spl-transaction'
 
 import type { ClusterWallet } from './portfolio-routes-loaded.js'
 
 export function useCreateAndSendSplTransaction(props: ClusterWallet) {
+  const { cluster, wallet } = props
+  const queryClient = useQueryClient()
   const client = useSolanaClient({ cluster: props.cluster })
 
   return useMutation({
@@ -30,6 +35,17 @@ export function useCreateAndSendSplTransaction(props: ClusterWallet) {
       const sender = await createKeyPairSignerFromJson({ json: wallet.secretKey })
 
       return createAndSendSplTransaction(client, { amount, decimals, destination, mint, sender })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getBalanceQueryOptions({ client, cluster, wallet }).queryKey,
+      })
+      queryClient.invalidateQueries({
+        queryKey: getAccountInfoQueryOptions({ client, cluster, wallet }).queryKey,
+      })
+      queryClient.invalidateQueries({
+        queryKey: getTokenAccountsQueryOptions({ client, cluster, wallet }).queryKey,
+      })
     },
   })
 }
--- packages/solana-client-react/src/use-get-token-accounts.tsx
+++ packages/solana-client-react/src/use-get-token-accounts.tsx
@@ -1,16 +1,29 @@
 import type { Cluster } from '@workspace/db/entity/cluster'
 import type { Wallet } from '@workspace/db/entity/wallet'
+import type { SolanaClient } from '@workspace/solana-client/solana-client'
 
-import { useQuery } from '@tanstack/react-query'
+import { queryOptions, useQuery } from '@tanstack/react-query'
 import { getTokenAccounts } from '@workspace/solana-client/get-token-accounts'
 
 import { useSolanaClient } from './use-solana-client'
 
-export function useGetTokenAccounts({ cluster, wallet }: { cluster: Cluster; wallet: Wallet }) {
-  const client = useSolanaClient({ cluster })
-
-  return useQuery({
+export function getTokenAccountsQueryOptions({
+  client,
+  cluster,
+  wallet,
+}: {
+  client: SolanaClient
+  cluster: Cluster
+  wallet: Wallet
+}) {
+  return queryOptions({
     queryFn: () => getTokenAccounts(client, { address: wallet.publicKey }),
     queryKey: ['get-token-accounts', { cluster, publicKey: wallet?.publicKey }],
   })
+}
+
+export function useGetTokenAccounts({ cluster, wallet }: { cluster: Cluster; wallet: Wallet }) {
+  const client = useSolanaClient({ cluster })
+
+  return useQuery(getTokenAccountsQueryOptions({ client, cluster, wallet }))
 }
