99260cbe3e6b95905c7137a1dfab726685de1ac4
Bram Borggreve <beeman@beeman.nl>
2025-11-01 18:34:00 +0400

    chore(web): add vite-bundle-analyzer
    Add vite-bundle-analyzer to the web app.
    
    It's disabled by default.
    
    To run it:
    
    ```shell
    cd apps/web
    pnpm run build -- --analyze
    ```

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,8 +2,17 @@ import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { analyzer } from 'vite-bundle-analyzer'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [cloudflare(), react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    analyzer({
+      enabled: process.argv.includes('--analyze'),
+    }),
+    cloudflare(),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
 })
