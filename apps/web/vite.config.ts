117aa6858a496d23e9dfaa4dbef7ca8044cd6491
Bram Borggreve <beeman@beeman.nl>
2025-11-01 17:52:57 +0400

    chore(web): externalize dependencies to decrease the bundle size

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,28 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const externalDependencies = [
+  '@floating-ui',
+  '@radix-react',
+  '@solana',
+  '@tanstack',
+  '@wallet-standard',
+  'dexie',
+  'i18next',
+  'lucide-react',
+  'react',
+  'react-dom',
+  'react-router',
+  'sonner',
+  'tailwind-merge',
+  'zod',
+]
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: externalDependencies.map((dep) => new RegExp(`^${dep}(\\/.*)?$`)),
+    },
+  },
   plugins: [cloudflare(), react(), tailwindcss(), tsconfigPaths()],
 })
