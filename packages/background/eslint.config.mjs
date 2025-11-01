732e5f701f53379473cfe21374b532c3a2ab6481
Tobey Codes <tobey@tobey.codes>
2025-11-01 17:09:31 -0600

    chore: format files

--- packages/background/eslint.config.mjs
+++ packages/background/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@workspace/config-eslint/base";
+import { config } from '@workspace/config-eslint/base'
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default config
