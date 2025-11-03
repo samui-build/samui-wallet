a6d5ca73e28a366810f1547979b9d907a2ff572d
Tobey Codes <tobey@tobey.codes>
2025-11-03 10:28:33 -0600

    feat: replace prettier with biome

--- packages/config-eslint/vite.js
+++ packages/config-eslint/vite.js
@@ -1,5 +1,4 @@
 import js from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
 import pluginReact from 'eslint-plugin-react'
 import pluginReactHooks from 'eslint-plugin-react-hooks'
 import globals from 'globals'
@@ -15,7 +14,6 @@ import { config as baseConfig } from './base.js'
 export const viteConfig = [
   ...baseConfig,
   js.configs.recommended,
-  eslintConfigPrettier,
   ...tseslint.configs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
--- packages/keypair/src/convert-key-pair-to-json.spec.ts
+++ packages/keypair/src/convert-key-pair-to-json.spec.ts
@@ -11,7 +11,6 @@ describe('convert-key-pair-to-json', () => {
       const mnemonic = 'pill tomorrow foster begin walnut borrow virtual kick shift mutual shoe scatter'
       const keyPairSigner = await createKeyPairSignerFromBip39({ extractable: true, mnemonic })
       const cryptoKeyPair = keyPairSigner.keyPair
-      // prettier-ignore
       const expectedJson = `[186,78,68,54,63,205,1,141,2,89,45,80,77,168,215,120,56,57,72,222,50,140,31,236,254,35,208,163,138,186,225,18,67,194,241,235,28,5,209,235,248,58,150,42,218,71,43,177,183,62,55,96,216,41,59,146,121,132,223,24,39,109,3,163]`
 
       // ACT
