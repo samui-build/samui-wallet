2243ac0fb2daeba3eedc3ece1ac2e2096f18e615
AtoMicKraK1n <prakharsharma2506@gmail.com>
2025-11-03 13:20:35 +0530

    refactor: used use-get-balance hook instead of rpc call

--- packages/portfolio/src/use-create-and-send-sol-transaction.tsx
+++ packages/portfolio/src/use-create-and-send-sol-transaction.tsx
@@ -3,7 +3,7 @@ import type { Wallet } from '@workspace/db/entity/wallet'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createKeyPairSignerFromJson } from '@workspace/keypair/create-key-pair-signer-from-json'
 import { getAccountInfoQueryOptions } from '@workspace/solana-client-react/use-get-account-info'
-import { getBalanceQueryOptions } from '@workspace/solana-client-react/use-get-balance'
+import { getBalanceQueryOptions, useGetBalance } from '@workspace/solana-client-react/use-get-balance'
 import { useSolanaClient } from '@workspace/solana-client-react/use-solana-client'
 import { createAndSendSolTransaction } from '@workspace/solana-client/create-and-send-sol-transaction'
 
@@ -13,22 +13,25 @@ export function useCreateAndSendSolTransaction(props: ClusterWallet) {
   const { cluster, wallet } = props
   const queryClient = useQueryClient()
   const client = useSolanaClient({ cluster: props.cluster })
+  const { data: balanceData } = useGetBalance({ cluster, wallet })
 
   return useMutation({
     mutationFn: async ({ amount, destination, wallet }: { amount: string; destination: string; wallet: Wallet }) => {
       if (!wallet.secretKey) {
         throw new Error(`No secret key for this wallet`)
       }
+
       const sender = await createKeyPairSignerFromJson({ json: wallet.secretKey })
 
-      const balance = await client.rpc.getBalance(sender.address).send()
-      const senderBalance = balance.value
+      if (!balanceData?.value) {
+        throw new Error('Balance not available')
+      }
 
       return createAndSendSolTransaction(client, {
         amount: BigInt(amount),
         destination,
         sender,
-        senderBalance,
+        senderBalance: balanceData.value,
       })
     },
     onSuccess: () => {
