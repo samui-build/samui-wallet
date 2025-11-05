1753bd06e6080d95c937528c3bec00dd6b8070a6
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2025-11-05 20:45:11 +0530

    fix(onboarding): persist experimental warning state across app lifecycle
    Update onboarding-feature-index.tsx
    
    Co-authored-by: tobey <3613405+tobeycodes@users.noreply.github.com>
    Signed-off-by: Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>

--- packages/onboarding/src/onboarding-feature-index.tsx
+++ packages/onboarding/src/onboarding-feature-index.tsx
@@ -1,19 +1,19 @@
+import { useDbPreference } from '@workspace/db-react/use-db-preference'
 import { useTranslation } from '@workspace/i18n'
 import { Button } from '@workspace/ui/components/button'
 import { UiExperimentalWarning } from '@workspace/ui/components/ui-experimental-warning'
-import { useState } from 'react'
 import { Link } from 'react-router'
 
 export function OnboardingFeatureIndex() {
   const { t } = useTranslation()
-  const [accepted, setAccepted] = useState(false)
+  const [accepted, setAccepted] = useDbPreference('warningAcceptExperimental')
   return (
     <div className="flex flex-col gap-6 items-center min-w-[400px]">
       <div className="flex flex-col items-center space-y-2">
         <div className="text-2xl">Welcome to 🏝️ Samui Wallet</div>
         <div className="text-lg text-muted-foreground">{t(($) => $['We hope you enjoy your stay!'])}</div>
       </div>
-      {accepted ? null : <UiExperimentalWarning close={() => setAccepted(true)} />}
+      {accepted === 'true' ? null : <UiExperimentalWarning close={() => setAccepted('true')} />}
       <div className="flex flex-col space-y-2 w-full">
         <Button asChild>
           <Link to="generate">Create a new wallet</Link>
