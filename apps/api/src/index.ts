cd1e5fc2c8b58d2c62f0a542f4214fafdb182f8e
Bram Borggreve <beeman@beeman.nl>
2025-11-06 18:54:24 +0400

    feat(api): enable CORS middleware

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,14 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Cloudflare.Env }>()
+
+app.use('*', async (c, next) => {
+  const origin = c.env.CORS_ORIGINS?.split(',').map((origin) => origin.trim()) ?? []
+
+  const handler = cors({ origin })
+  return handler(c, next)
+})
 
 app.get('/', (c) => {
   return c.text('Samui Wallet')
