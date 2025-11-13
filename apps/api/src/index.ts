0cbc6b7f0950bee8c0b56392241893b867eb035c
Tobey Codes <tobey@tobey.codes>
2025-11-13 12:42:47 -0800

    refactor(api): use effect

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,17 +1,29 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import { HttpApi, HttpApiBuilder, HttpApiEndpoint, HttpApiGroup, HttpServer } from '@effect/platform'
+import { Effect, Layer, Schema } from 'effect'
 
-const app = new Hono<{ Bindings: Cloudflare.Env }>()
+const MyApi = HttpApi.make('MyApi').add(
+  HttpApiGroup.make('HelloWorld').add(HttpApiEndpoint.get('hello-world')`/`.addSuccess(Schema.String)),
+)
 
-app.use('*', async (c, next) => {
-  const origin = c.env.CORS_ORIGINS?.split(',').map((origin) => origin.trim()) ?? []
+const HelloWorldLive = HttpApiBuilder.group(MyApi, 'HelloWorld', (handlers) =>
+  handlers.handle('hello-world', () => Effect.succeed('Hello, World!')),
+)
 
-  const handler = cors({ origin })
-  return handler(c, next)
-})
+const MyApiLive = HttpApiBuilder.api(MyApi).pipe(Layer.provide(HelloWorldLive))
 
-app.get('/', (c) => {
-  return c.text('Samui Wallet')
-})
-
-export default app
+export default {
+  fetch: (request: Request, env: Cloudflare.Env) => {
+    const { handler } = HttpApiBuilder.toWebHandler(
+      Layer.mergeAll(
+        MyApiLive,
+        HttpApiBuilder.middlewareCors({
+          allowedOrigins: env.CORS_ORIGINS?.split(',').map((origin) => origin.trim()) ?? [],
+          credentials: true,
+          maxAge: 86400,
+        }),
+        HttpServer.layerContext,
+      ),
+    )
+    return handler(request)
+  },
+}
