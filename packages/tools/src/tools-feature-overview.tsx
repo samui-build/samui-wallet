5deae779b7cf45d41baf135962815a158c56751d
readylayerone_ <datasalaryman@protonmail.com>
2025-11-14 17:52:50 +0800

    fix(tools): consistent content container (#424)
    Co-authored-by: beeman <beeman@beeman.nl>

--- packages/tools/src/tools-feature-overview.tsx
+++ packages/tools/src/tools-feature-overview.tsx
@@ -3,7 +3,7 @@ import { ToolsUiOverview } from './ui/tools-ui-overview.tsx'
 
 export default function ToolsFeatureOverview() {
   return (
-    <div className="space-y-6 p-3">
+    <div className="space-y-6">
       <ToolsUiOverview tools={tools.filter((t) => !t.comingSoon)} />
       <ToolsUiOverview tools={tools.filter((t) => t.comingSoon)} />
     </div>
