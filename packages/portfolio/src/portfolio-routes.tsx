e201059942b893ad8517d1d2286168f23534f754
Data Salaryman <datasalaryman@protonmail.com>
2025-11-14 17:35:05 +0800

    fix(portfolio): consistent content container

--- packages/portfolio/src/portfolio-routes.tsx
+++ packages/portfolio/src/portfolio-routes.tsx
@@ -1,17 +1,20 @@
+import { UiPage } from '@workspace/ui/components/ui-page'
 import { PortfolioRoutesLoaded } from './portfolio-routes-loaded.tsx'
 import { PortfolioUiAccountGuard } from './ui/portfolio-ui-account-guard.tsx'
 import { PortfolioUiNetworkGuard } from './ui/portfolio-ui-network-guard.tsx'
 
 export default function PortfolioRoutes() {
   return (
-    <PortfolioUiNetworkGuard
-      render={({ network }) => (
-        <PortfolioUiAccountGuard
-          render={({ account }) => {
-            return <PortfolioRoutesLoaded account={account} network={network} />
-          }}
-        />
-      )}
-    />
+    <UiPage containerProps={{ className: 'sm:max-w-2xl' }}>
+      <PortfolioUiNetworkGuard
+        render={({ network }) => (
+          <PortfolioUiAccountGuard
+            render={({ account }) => {
+              return <PortfolioRoutesLoaded account={account} network={network} />
+            }}
+          />
+        )}
+      />
+    </UiPage>
   )
 }
