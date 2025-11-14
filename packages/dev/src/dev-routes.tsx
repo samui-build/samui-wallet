b56efea810459976e28d27e48d85be634d4b1e61
Data Salaryman <datasalaryman@protonmail.com>
2025-11-14 17:18:50 +0800

    fix(dev): consistent content container

--- packages/dev/src/dev-routes.tsx
+++ packages/dev/src/dev-routes.tsx
@@ -1,6 +1,11 @@
+import { UiPage } from '@workspace/ui/components/ui-page'
 import { UiTabRoutes } from '@workspace/ui/components/ui-tab-routes'
 import { devFeatures } from './dev-features.tsx'
 
 export default function DevRoutes() {
-  return <UiTabRoutes basePath="/dev" className="mt-2 mb-4 lg:mb-6" tabs={devFeatures} />
+  return (
+    <UiPage>
+      <UiTabRoutes basePath="/dev" className="mt-2 mb-4 lg:mb-6" tabs={devFeatures} />
+    </UiPage>
+  )
 }
