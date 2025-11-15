166e073b7d24ad49390833fada7a85763e6d3f09
beeman <beeman@beeman.nl>
2025-11-15 15:47:49 +0400

    refactor(ui): get rid of ui-container component

--- packages/onboarding/src/ui/onboarding-ui-layout.tsx
+++ packages/onboarding/src/ui/onboarding-ui-layout.tsx
@@ -1,12 +1,11 @@
-import { UiContainer } from '@workspace/ui/components/ui-container'
 import { Outlet } from 'react-router'
 
 export function OnboardingUiLayout() {
   return (
     <div className="min-h-full w-full flex items-center justify-center">
-      <UiContainer className="sm:max-w-xl">
+      <div className="w-full sm:max-w-lg">
         <Outlet />
-      </UiContainer>
+      </div>
     </div>
   )
 }
