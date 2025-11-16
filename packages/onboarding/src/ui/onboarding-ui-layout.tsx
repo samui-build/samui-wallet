852c519f0564f320955d3dea871e34fcbb01f5c8
Bram Borggreve <beeman@beeman.nl>
2025-11-17 00:39:36 +0400

    fix(onboarding): ensure small viewport has some padding

--- packages/onboarding/src/ui/onboarding-ui-layout.tsx
+++ packages/onboarding/src/ui/onboarding-ui-layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 export function OnboardingUiLayout() {
   return (
     <div className="min-h-full w-full flex items-center justify-center">
-      <div className="w-full sm:max-w-lg">
+      <div className="w-full px-2 sm:px-0 max-w-lg">
         <Outlet />
       </div>
     </div>
