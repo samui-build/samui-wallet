2f4e249a6682689060db910093636a3e7342f807
Data Salaryman <datasalaryman@protonmail.com>
2025-11-18 18:48:49 +0800

    fix: restore styles when settings nav item is in active state

--- packages/settings/src/ui/settings-ui-page-item.tsx
+++ packages/settings/src/ui/settings-ui-page-item.tsx
@@ -1,22 +1,24 @@
 import { Item, ItemActions, ItemContent, ItemMedia, ItemTitle } from '@workspace/ui/components/item'
 import { UiIcon } from '@workspace/ui/components/ui-icon'
-import { Link } from 'react-router'
+import { NavLink } from 'react-router'
 import type { SettingsPage } from '../data-access/settings-page.ts'
 
 export function SettingsUiPageItem({ page }: { page: SettingsPage }) {
   return (
-    <Item asChild key={page.id} size="sm" variant="outline">
-      <Link to={`/settings/${page.id}`}>
-        <ItemMedia>
-          <UiIcon icon={page.icon} />
-        </ItemMedia>
-        <ItemContent>
-          <ItemTitle className="text-xl">{page.name}</ItemTitle>
-        </ItemContent>
-        <ItemActions>
-          <UiIcon className="size-4" icon="chevronRight" />
-        </ItemActions>
-      </Link>
-    </Item>
+    <NavLink to={`/settings/${page.id}`}>
+      {({ isActive }) => (
+        <Item size="sm" variant={isActive ? 'muted' : 'outline'}>
+          <ItemMedia>
+            <UiIcon icon={page.icon} />
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle className="text-xl">{page.name}</ItemTitle>
+          </ItemContent>
+          <ItemActions>
+            <UiIcon className="size-4" icon="chevronRight" />
+          </ItemActions>
+        </Item>
+      )}
+    </NavLink>
   )
 }
