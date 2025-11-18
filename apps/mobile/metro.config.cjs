a064beb3b712236a0ea2b200950f8d45d48b8102
beeman <beeman@beeman.nl>
2025-11-18 07:45:35 +0000

    feat(mobile): configure uniwind (#461)
    Co-authored-by: JuanRdBo <juan@decaf.so>

---
+++ apps/mobile/metro.config.cjs
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require('expo/metro-config')
+const { withUniwindConfig } = require('uniwind/metro')
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname)
+
+/** @type {import('expo/metro-config').MetroConfig} */
+module.exports = withUniwindConfig(config, {
+  cssEntryFile: './src/global.css',
+  dtsFile: './src/uniwind-types.d.ts',
+})
