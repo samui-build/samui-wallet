02ef8bff0cf5c038fcc5548aa114077512c0c554
Tobey Codes <tobey@tobey.codes>
2025-11-18 21:52:32 -0600

    fix: dependabot

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,6 +1,6 @@
 import { setEnv } from '@workspace/env/env'
 
-setEnv({
+const env = {
   activeNetworkId: import.meta.env['VITE_ACTIVE_NETWORK_ID'],
   apiEndpoint: import.meta.env['VITE_API_ENDPOINT'],
   networkDevnet: import.meta.env['VITE_NETWORK_DEVNET'],
@@ -11,4 +11,6 @@ setEnv({
   networkMainnetSubscriptions: import.meta.env['VITE_NETWORK_MAINNET_SUBSCRIPTIONS'],
   networkTestnet: import.meta.env['VITE_NETWORK_TESTNET'],
   networkTestnetSubscriptions: import.meta.env['VITE_NETWORK_TESTNET_SUBSCRIPTIONS'],
-})
+}
+
+setEnv(Object.fromEntries(Object.entries(env).filter(([, value]) => value !== undefined)))
