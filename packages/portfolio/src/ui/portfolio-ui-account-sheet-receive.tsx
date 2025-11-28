d966b948eac8758c86dbe00080c63369fcb36a05
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-11-28 09:50:31 +0530

    refactor: added div again in portfolio receive sheet

--- packages/portfolio/src/ui/portfolio-ui-account-sheet-receive.tsx
+++ packages/portfolio/src/ui/portfolio-ui-account-sheet-receive.tsx
@@ -19,15 +19,18 @@ export function PortfolioUiAccountSheetReceive({ account }: { account: Account }
         </Button>
       }
     >
-      <UiTextCopyButton
-        label={t(($) => $.actionCopyPublicKey)}
-        text={account.publicKey}
-        toast={t(($) => $.actionCopyPublicKeySuccess)}
-        toastFailed={t(($) => $.actionCopyPublicKeyError)}
-        variant="outline"
-      />
-      <div className="px-6">
-        <UiQrCode content={account.publicKey} />
+      <div className="space-y-6 p-2 text-center md:pb-10">
+        <UiTextCopyButton
+          label={t(($) => $.actionCopyPublicKey)}
+          text={account.publicKey}
+          toast={t(($) => $.actionCopyPublicKeySuccess)}
+          toastFailed={t(($) => $.actionCopyPublicKeyError)}
+        />
+        <div className="flex size-85 w-full justify-center">
+          <div className="aspect-square">
+            <UiQrCode content={account.publicKey} />
+          </div>
+        </div>
       </div>
     </UiBottomSheet>
   )
