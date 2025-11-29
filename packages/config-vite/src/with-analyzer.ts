33bc397e31a68b8fcdf531ed975c17175915cd29
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-11-30 00:25:20 +0530

    fix(config-vite): use ReturnType for withAnalyzer return type

--- packages/config-vite/src/with-analyzer.ts
+++ packages/config-vite/src/with-analyzer.ts
@@ -1,5 +1,5 @@
 import { analyzer } from 'vite-bundle-analyzer'
 
-export function withAnalyzer(enabled = !!process.env['ANALYZE']) {
+export function withAnalyzer(enabled = !!process.env['ANALYZE']): ReturnType<typeof analyzer> {
   return analyzer({ enabled })
 }
