fb257b7f4f04057201e8a2c736439ba8431830a9
Shubham <thatcoderguyshubham@gmail.com>
2025-11-30 18:13:01 +0530

    feat: add shared vite configuration package (#625)
    Co-authored-by: Tobey Codes <tobey@tobey.codes>

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,32 +1,26 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { sharedConfig } from '@workspace/config-vite'
+import { defineConfig, mergeConfig } from 'vite'
 
 const host = process.env['TAURI_DEV_HOST']
 
-export default defineConfig({
-  clearScreen: false,
-  plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
+export default mergeConfig(
+  sharedConfig,
+  defineConfig({
+    clearScreen: false,
+    server: {
+      hmr: host
+        ? {
+            host,
+            port: 1421,
+            protocol: 'ws',
+          }
+        : false,
+      host: host || false,
+      port: 1420,
+      strictPort: true,
+      watch: {
+        ignored: ['**/src-tauri/**'],
       },
-    }),
-    tailwindcss(),
-  ],
-  server: {
-    hmr: host
-      ? {
-          host,
-          port: 1421,
-          protocol: 'ws',
-        }
-      : false,
-    host: host || false,
-    port: 1420,
-    strictPort: true,
-    watch: {
-      ignored: ['**/src-tauri/**'],
     },
-  },
-})
+  }),
+)
