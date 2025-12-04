044bd515f58a5f9b58efde3c4ad7691227fe7f9a
Tobey Codes <tobey@tobey.codes>
2025-12-04 22:32:31 +0400

    feat(wallet-standard): add standard events

--- packages/wallet-standard/src/features/events.ts
+++
@@ -1,8 +0,0 @@
-import type { StandardEventsListeners, StandardEventsNames } from '@wallet-standard/core'
-
-export function on<T extends StandardEventsNames>(event: T, listener: StandardEventsListeners[T]): () => void {
-  console.log('on called', event, listener)
-  return () => {
-    console.log('off called')
-  }
-}
