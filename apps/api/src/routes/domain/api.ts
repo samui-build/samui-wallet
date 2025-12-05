91e89864b675f39ceedf9141ba713b2ce9b4b14b
Tobey Codes <tobey@tobey.codes>
2025-12-05 23:34:10 +0400

    feat(api): add domain search endpoint

--- apps/api/src/routes/domain/api.ts
+++ apps/api/src/routes/domain/api.ts
@@ -1,8 +1,22 @@
 import { HttpApiEndpoint, HttpApiGroup, OpenApi } from '@effect/platform'
 import { Schema } from 'effect'
+import {
+  SnsDomainNotFound,
+  SnsInvalidDomainName,
+  SnsSearchResult,
+  SnsServiceUnavailable,
+} from '../../services/sns/service.ts'
+
+export const Domain = Schema.String.pipe(Schema.brand('Domain'))
+
+export type Domain = typeof Domain.Type
 
 export class DomainApi extends HttpApiGroup.make('Domain').add(
-  HttpApiEndpoint.get('domainSearch', '/domain/search')
+  HttpApiEndpoint.get('domainSearch', '/domain/:domain')
     .annotate(OpenApi.Summary, 'Search domain name')
-    .addSuccess(Schema.String),
+    .setPath(Schema.Struct({ domain: Domain }))
+    .addSuccess(SnsSearchResult)
+    .addError(SnsDomainNotFound, { status: 404 })
+    .addError(SnsInvalidDomainName, { status: 400 })
+    .addError(SnsServiceUnavailable, { status: 503 }),
 ) {}
