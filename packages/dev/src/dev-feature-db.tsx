5066f11b9a4601d2d4be4c9af1594b9208ed79d9
Tobey Codes <tobey@tobey.codes>
2025-12-10 02:21:37 +0400

    refactor: remove dev feature db

--- packages/dev/src/dev-feature-db.tsx
+++
@@ -1,9 +0,0 @@
-import { DevFeatureDbTables } from './dev-feature-db-tables.tsx'
-
-export default function DevFeatureDb() {
-  return (
-    <div className="space-y-6">
-      <DevFeatureDbTables />
-    </div>
-  )
-}
