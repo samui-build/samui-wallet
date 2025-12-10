9ae26ed17ea7103a6912a727314ae171ef65b7ab
Tobey Codes <tobey@tobey.codes>
2025-12-10 12:09:53 +0400

    feat(flags): move enable development mode to feature flags

--- packages/shell/src/ui/shell-ui-developer-mode-link.tsx
+++ packages/shell/src/ui/shell-ui-developer-mode-link.tsx
@@ -1,12 +1,10 @@
-import { useSetting } from '@workspace/db-react/use-setting'
+import { isEnabled } from '@workspace/flags'
 import { Button } from '@workspace/ui/components/button'
 import { UiIcon } from '@workspace/ui/components/ui-icon'
 import { Link } from 'react-router'
 
 export function ShellUiDeveloperModeLink() {
-  const [developerModeEnabled] = useSetting('developerModeEnabled')
-
-  return developerModeEnabled === 'true' ? (
+  return isEnabled('developerMode') ? (
     <Button asChild size="icon" variant="outline">
       <Link to="/dev">
         <UiIcon icon="bug" />
