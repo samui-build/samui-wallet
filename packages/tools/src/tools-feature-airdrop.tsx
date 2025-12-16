5a4987224ccaa8520e36c3791dfb6c7cc7c29139
beeman <beeman@beeman.nl>
2025-12-16 20:15:39 +0700

    feat(tools): show table of airdrop results with link to explorer

--- packages/tools/src/tools-feature-airdrop.tsx
+++ packages/tools/src/tools-feature-airdrop.tsx
@@ -1,19 +1,25 @@
-import { address } from '@solana/kit'
+import { type Address, address, type Signature } from '@solana/kit'
 import type { Account } from '@workspace/db/account/account'
 import type { Network } from '@workspace/db/network/network'
 import { useWalletLive } from '@workspace/db-react/use-wallet-live'
 import { ExplorerUiExplorerLink } from '@workspace/explorer/ui/explorer-ui-explorer-link'
+import { ExplorerUiLinkAddress } from '@workspace/explorer/ui/explorer-ui-link-address'
+import { ExplorerUiLinkSignature } from '@workspace/explorer/ui/explorer-ui-link-signature'
 import { solToLamports } from '@workspace/solana-client/sol-to-lamports'
 import { useRequestAirdrop } from '@workspace/solana-client-react/use-request-airdrop'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@workspace/ui/components/table'
 import { UiCard } from '@workspace/ui/components/ui-card'
 import { toastSuccess } from '@workspace/ui/lib/toast-success'
+import { useState } from 'react'
 import { ToolsUiAirdropForm } from './ui/tools-ui-airdrop-form.tsx'
 
 export default function ToolsFeatureAirdrop(props: { account: Account; network: Network }) {
   const wallets = useWalletLive()
   const { isPending, mutateAsync } = useRequestAirdrop(props.network)
+  const [results, setResults] = useState<AirdropResult[]>([])
+
   return (
-    <UiCard backButtonTo="/tools" title="Airdrop">
+    <UiCard backButtonTo="/tools" contentProps={{ className: 'space-y-2 md:space-y-6' }} title="Airdrop">
       <ToolsUiAirdropForm
         disabled={isPending}
         submit={async (input) => {
@@ -21,6 +27,14 @@ export default function ToolsFeatureAirdrop(props: { account: Account; network: 
             address: address(input.address),
             amount: solToLamports(input.amount.toString()),
           })
+          setResults((result) => [
+            ...result,
+            {
+              address: address(input.address),
+              amount: input.amount,
+              signature,
+            },
+          ])
           toastSuccess(
             <ExplorerUiExplorerLink
               label="View on explorer"
@@ -32,6 +46,43 @@ export default function ToolsFeatureAirdrop(props: { account: Account; network: 
         }}
         wallets={wallets}
       />
+      <ToolsUiAirdropResultTable results={results} />
     </UiCard>
+  )
+}
+
+interface AirdropResult {
+  address: Address
+  amount: number
+  signature: Signature
+}
+function ToolsUiAirdropResultTable({ results = [] }: { results: AirdropResult[] }) {
+  if (!results.length) {
+    return null
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow className="hover:bg-transparent">
+          <TableHead>Address</TableHead>
+          <TableHead>Signature</TableHead>
+          <TableHead className="w-[120px] text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {results.map(({ address, amount, signature }) => (
+          <TableRow className="hover:bg-transparent" key={signature}>
+            <TableCell>
+              <ExplorerUiLinkAddress address={address} basePath="/explorer" />
+            </TableCell>
+            <TableCell>
+              <ExplorerUiLinkSignature basePath="/explorer" signature={signature} />
+            </TableCell>
+            <TableCell className="w-[120px] text-right">{amount} SOL</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
