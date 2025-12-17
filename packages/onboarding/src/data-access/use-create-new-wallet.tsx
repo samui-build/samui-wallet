5d97d070a6db39b480599f111097d929c30d6c7d
beeman <beeman@beeman.nl>
2025-12-17 16:40:35 +0700

    fix(onboarding): determine new wallet name based on existing wallet

--- packages/onboarding/src/data-access/use-create-new-wallet.tsx
+++ packages/onboarding/src/data-access/use-create-new-wallet.tsx
@@ -1,16 +1,18 @@
 import { derivationPaths } from '@workspace/keypair/derivation-paths'
+import { useDetermineWalletName } from '@workspace/settings/data-access/use-determine-wallet-name'
 import { useGenerateWalletWithAccountMutation } from '@workspace/settings/data-access/use-generate-wallet-with-account-mutation'
 import { toastError } from '@workspace/ui/lib/toast-error'
 import { toastSuccess } from '@workspace/ui/lib/toast-success'
 
 export function useCreateNewWallet() {
   const mutation = useGenerateWalletWithAccountMutation()
+  const name = useDetermineWalletName()
   return (mnemonic: string) =>
     mutation
       .mutateAsync({
         derivationPath: derivationPaths.default,
         mnemonic,
-        name: 'Wallet 1',
+        name,
         secret: '',
       })
       .then(() => {
